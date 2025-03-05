55d05e7db644ea400acb6d691b50971a794dfae0
Snusii <Snusii1995@gmail.com>
2025-03-05 22:25:49 +0100

    Some html structure with styleing

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 
-
+/*const hobbies = [
+    "Koding",
+    "Gaming",
+    "Baking",
+    "Dyr", 
+    "Strikking",
+    "Musikk"
+]
 
 
 //////////////
 // Functions
 /////////////
 
+function downloadCV () {
+
+}
+
+function darkMode () {
+
+}
+
+function hobbyChanger () {
+    for (let i = 0; i < hobbies.length; i++)
+
+
+}
+
+function contactMe () {
+
+}
+*/
