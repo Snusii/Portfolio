7f3625dd1a813e109776ea07f556fc93a97b847a
Snusii <Snusii1995@gmail.com>
2025-03-07 00:46:07 +0100

    Made a darkmode for the page

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+const toggleSwitch = document.querySelector("#toggleSwitch");
+let lightMode = false;
+
+
+const setDarkMode = () => {
+    document.body.classList.remove("light-mode")
+    lightMode = false;
+};
+
+const setLightMode = () => {
+    document.body.classList.add("light-mode")
+    lightMode = true;
+};
+
+
+
+function toggle() {
+    lightMode ? setDarkMode() : setLightMode();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*const hobbies = [
     "Koding",
