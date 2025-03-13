6f6309bb6c3cb6fa99dec9c957b13215c615748b
Snusii <Snusii1995@gmail.com>
2025-03-13 20:06:14 +0100

    implemented textTyper function with the use of promise

--- script.js
+++ script.js
@@ -18,19 +18,41 @@ function toggle() {
 }
 
 
-const headerText = document.querySelector("#header-text")
 
-function textTyperDelay (duration) {
-    return new Promise((resolve, duration) => {
-        
-})
 
-async function textTyper (text, delay) {
-    for (let i = 0; i < text.length; i++) {
-        await textTyper(delay)
+// textTyper function call
+textTyper (document.querySelector("#header-text"))
+
+// textTyper async function takes in html element that contains text and writes every character with a delay
+async function textTyper (element) {
+
+    // getting the text value from the element and it into text variable
+    let text = element.textContent;
+
+    // setting the element to the first character, to prevent DOM movement
+    element.textContent = text[0];
+
+    // setting the text builder variable to the same character
+    let headerText = text[0];
+
+    // looping through the text, starting at position 1 (first character already set)
+    for (let i = 1; i < text.length; i++) {
+        // adding current character from the text to headerText
+        headerText += text[i];
+
+        // creating a promise
+        // with only resolve posibility 
+        let myPromise = new Promise((resolve) => {
+            // setting timeout 
+            setTimeout(function() {
+                resolve(headerText);
+            },350);
+        }); 
+
+        // awaiting timout then updates element
+        await myPromise.then((value) => {
+            element.textContent = value;
+        }); 
     }
 }
-}
-
-const delay = 1000;
 
